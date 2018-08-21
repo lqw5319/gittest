a8727239c622819b7a77b6409d459757433e012f
MrAta <immrata@gmail.com>
2018-08-21 10:19:15 -0400

    revised prog1

--- prog1/prog1.c
+++ prog1/prog1.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void allocate(int count)
-{
-  int x[300000];
-  char *c;
 
-  if(count >= 0)
-  {
-    c = malloc (30000);
-    printf("Stack Address  = %p    Heap Address = %p\n\n",x,c);
-    allocate(count - 1);
-  }
-}
 
 int main(int argc, char const *argv[]) {
-  int count = 10;
+  int *p;
+   p = malloc(sizeof(int));
+   int i=0;
+     
+while(1){
+       printf("Value at address %p is %d\n",p, p[i]);
+      p[i] = i*i;
+i++;
+     }
 
-  printf("Entering Function \n\n");
-  allocate(count);
-  printf("Press Enter to continue\n");
-  getchar();
-  printf("Program Terminated Successfully\n");
   return 0;
 }
